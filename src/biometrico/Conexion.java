7453777773a159a301fd41b0c638b6b7a9123eeb
Erik Granda <egranda3756@uta.edu.ec>
2022-07-07 21:27:15 -0500

    Clase para conectarse a la BD

---
+++ src/biometrico/Conexion.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biometrico;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Erik
+ */
+public class Conexion {
+  Connection con=null;
+    public Connection  conexion(){ 
+    try{
+        Class.forName("com.mysql.jdbc.Driver");
+        con=DriverManager.getConnection("jdbc:mysql://localhost/proyectocvprueba", "root", "");
+        System.out.println("conexion establecida");
+    }  catch (ClassNotFoundException | SQLException e) { 
+           System.out.println("error de conexion"+e);
+           JOptionPane.showMessageDialog(null, "Error de conexion con la base de datos."+e);
+           }
+       return con;
+    }  
+}
