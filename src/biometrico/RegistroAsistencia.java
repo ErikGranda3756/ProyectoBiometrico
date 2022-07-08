452e77d63e60e693c2ded7a976e9302c8c2918d2
Erik Granda <egranda3756@uta.edu.ec>
2022-07-07 21:30:14 -0500

    Modificación de la interfaz de registro de asistencia

--- src/biometrico/RegistroAsistencia.java
+++ src/biometrico/RegistroAsistencia.java
@@ -5,17 +5,138 @@
  */
 package biometrico;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+
 /**
  *
  * @author Erik
  */
-public class RegistroAsistencia extends javax.swing.JFrame {
+public class RegistroAsistencia extends javax.swing.JFrame implements Runnable {
+
+    DefaultTableModel modelo;
+    DefaultTableCellRenderer tcr;
+    String hora, ampm, fecha, horac;
+    Thread hilo;
+    Calendar calendario;
+    Date fechahora, horaMax;
+    Conexion con = new Conexion();
+    Connection reg = con.conexion();
 
     /**
      * Creates new form RegistroAsistencia
      */
     public RegistroAsistencia() {
         initComponents();
+        empezarHilo();
+        cargarTabla();
+        llenartablaInfoDocente();
+        //activarBotones();
+//verificarHorario();
+
+    }
+
+//    public void activarBotones() {
+//        horaMax = generarHora("41");
+//        while (fechahora.equals(horaMax)) {
+//            jbtnMarcarAsistencia.setEnabled(true);
+//        }
+//        jbtnMarcarAsistencia.setEnabled(false);
+//        
+//    }
+    public void cargarTabla() {
+        tcr = new DefaultTableCellRenderer();
+        modelo = new DefaultTableModel();
+        modelo.addColumn("CEDULA");
+        modelo.addColumn("TIPO REGISTRO");
+        modelo.addColumn("ENTRADA JORNADA");
+        modelo.addColumn("SALIDA JORNADA ");
+        TableCellRenderer rendererFromHeader = jtblUsuarioLogin.getTableHeader().getDefaultRenderer();
+        JLabel headerLabel = (JLabel) rendererFromHeader;
+        headerLabel.setHorizontalAlignment(JLabel.CENTER);
+        jtblUsuarioLogin.setModel(modelo);
+        tcr.setHorizontalAlignment(SwingConstants.CENTER);
+        jtblUsuarioLogin.getColumnModel().getColumn(0).setCellRenderer(tcr);
+        jtblUsuarioLogin.getColumnModel().getColumn(1).setCellRenderer(tcr);
+        jtblUsuarioLogin.getColumnModel().getColumn(2).setCellRenderer(tcr);
+        jtblUsuarioLogin.getColumnModel().getColumn(3).setCellRenderer(tcr);
+    }
+
+    public void empezarHilo() {
+        hilo = new Thread(this);
+        hilo.start();
+    }
+
+    @Override
+    public void run() {
+        Thread ct = Thread.currentThread();
+
+        while (ct == hilo) {
+            calcula();
+            //jlblReloj.setText(hora + " " + ampm);
+            jlblReloj.setText(hora);
+            horac = hora;
+            try {
+                Thread.sleep(100);
+            } catch (Exception e) {
+            }
+
+        }
+    }
+
+    private void calcula() {
+        Calendar calendario = Calendar.getInstance();
+        fechahora = calendario.getTime();
+        SimpleDateFormat forhora = new SimpleDateFormat("HH  :  mm  :  ss");
+        calendario.setTime(fechahora);
+        //ampm = calendario.get(Calendar.AM_PM) == Calendar.AM ? "AM" : "PM";
+        hora = forhora.format(fechahora);
+    }
+
+//    public Date generarHora(String min) {
+//        
+//        try {
+//            SimpleDateFormat forhora = new SimpleDateFormat("hh:mm");
+//            Date horaNueva = forhora.parse("19:" + min);
+//            return horaNueva;
+//        } catch (ParseException ex) {
+//            JOptionPane.showMessageDialog(null, ex);
+//        }
+//        return fechahora;
+//    }
+    public void llenartablaInfoDocente() {
+        String sql = "";
+        sql = "SELECT d.ced_doc,j.ent_jor,j.sal_jor from docentes as d,jornadas as j where j.doc_asi=" + Docente.cedula;
+        String[] datos = new String[4];
+        try {
+            Statement st = reg.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+
+            while (rs.next()) {
+                datos[0] = rs.getString("d.ced_doc");
+                datos[2] = rs.getString("j.ent_jor");
+                datos[3] = rs.getString("j.sal_jor");
+                modelo.addRow(datos);
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        }
     }
 
     /**
@@ -31,12 +152,12 @@ public class RegistroAsistencia extends javax.swing.JFrame {
         jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jPanel3 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        jlblReloj = new javax.swing.JLabel();
+        jbtnMarcarAsistencia = new javax.swing.JButton();
+        jbtnCancelar = new javax.swing.JButton();
         jPanel4 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        jtblUsuarioLogin = new javax.swing.JTable();
         jLabel3 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTable2 = new javax.swing.JTable();
@@ -67,10 +188,10 @@ public class RegistroAsistencia extends javax.swing.JFrame {
         jPanel3.setBackground(new java.awt.Color(245, 117, 117));
         jPanel3.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 3, true));
 
-        jLabel2.setFont(new java.awt.Font("Leelawadee", 1, 48)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel2.setText("10  :  30  :  59");
+        jlblReloj.setFont(new java.awt.Font("Leelawadee", 1, 60)); // NOI18N
+        jlblReloj.setForeground(new java.awt.Color(255, 255, 255));
+        jlblReloj.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jlblReloj.setText("10  :  30  :  59");
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
@@ -78,39 +199,44 @@ public class RegistroAsistencia extends javax.swing.JFrame {
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jlblReloj, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 97, Short.MAX_VALUE)
+                .addComponent(jlblReloj, javax.swing.GroupLayout.DEFAULT_SIZE, 97, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
-        jButton1.setBackground(java.awt.Color.green);
-        jButton1.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("Registrar Salida");
+        jbtnMarcarAsistencia.setBackground(java.awt.Color.green);
+        jbtnMarcarAsistencia.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        jbtnMarcarAsistencia.setForeground(new java.awt.Color(255, 255, 255));
+        jbtnMarcarAsistencia.setText("Registrar Salida");
+        jbtnMarcarAsistencia.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtnMarcarAsistenciaActionPerformed(evt);
+            }
+        });
 
-        jButton2.setBackground(java.awt.Color.red);
-        jButton2.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
-        jButton2.setForeground(new java.awt.Color(255, 255, 255));
-        jButton2.setText("Cancelar");
+        jbtnCancelar.setBackground(java.awt.Color.red);
+        jbtnCancelar.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        jbtnCancelar.setForeground(new java.awt.Color(255, 255, 255));
+        jbtnCancelar.setText("Cancelar");
 
         jPanel4.setBackground(new java.awt.Color(255, 255, 255));
 
-        jTable1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jtblUsuarioLogin.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+        jtblUsuarioLogin.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {"1803459450", "Salida", "14:00", "17:00"}
             },
             new String [] {
-                "CEDULA", "TIPO REGISTRO", "ENTRADA JORNADA", "SALIDA JORNADA"
+                "CEDULASSSSS", "TIPO REGISTROSSSS", "ENTRADA JORNADASSS", "SALIDA JORNADA"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(jtblUsuarioLogin);
 
         jLabel3.setBackground(new java.awt.Color(102, 0, 0));
         jLabel3.setFont(new java.awt.Font("Times New Roman", 1, 13)); // NOI18N
@@ -148,7 +274,7 @@ public class RegistroAsistencia extends javax.swing.JFrame {
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
@@ -168,9 +294,9 @@ public class RegistroAsistencia extends javax.swing.JFrame {
                     .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(13, 13, 13)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jbtnMarcarAsistencia, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 79, Short.MAX_VALUE)
-                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jbtnCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(13, 13, 13)))
                 .addContainerGap())
         );
@@ -182,8 +308,8 @@ public class RegistroAsistencia extends javax.swing.JFrame {
                 .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, 58, Short.MAX_VALUE))
+                    .addComponent(jbtnMarcarAsistencia, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jbtnCancelar, javax.swing.GroupLayout.DEFAULT_SIZE, 58, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -206,6 +332,11 @@ public class RegistroAsistencia extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jbtnMarcarAsistenciaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnMarcarAsistenciaActionPerformed
+        // TODO add your handling code here:
+
+    }//GEN-LAST:event_jbtnMarcarAsistenciaActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -220,16 +351,24 @@ public class RegistroAsistencia extends javax.swing.JFrame {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
+
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(RegistroAsistencia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistroAsistencia.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(RegistroAsistencia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistroAsistencia.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(RegistroAsistencia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistroAsistencia.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(RegistroAsistencia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistroAsistencia.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
@@ -242,10 +381,7 @@ public class RegistroAsistencia extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
@@ -253,7 +389,10 @@ public class RegistroAsistencia extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTable jTable1;
     private javax.swing.JTable jTable2;
+    private javax.swing.JButton jbtnCancelar;
+    private javax.swing.JButton jbtnMarcarAsistencia;
+    private javax.swing.JLabel jlblReloj;
+    private javax.swing.JTable jtblUsuarioLogin;
     // End of variables declaration//GEN-END:variables
 }
